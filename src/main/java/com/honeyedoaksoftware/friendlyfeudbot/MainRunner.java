536bc4207470646e4f474ca41311eb3917d7099c
Pieter Van Eeckhout <pieter.vaneeckhout@docbyte.com>
2018-05-03 09:27:36 +0200

    adding initial commands, no functionality yet

---
+++ src/main/java/com/honeyedoaksoftware/friendlyfeudbot/MainRunner.java
@@ -0,0 +1,23 @@
+package com.honeyedoaksoftware.friendlyfeudbot;
+
+import sx.blah.discord.api.IDiscordClient;
+
+public class MainRunner {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
+            return;
+        }
+
+        IDiscordClient cli = BotUtils.getBuiltDiscordClient(args[0]);
+
+        // Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
+        cli.getDispatcher().registerListener(new MessageHandler());
+
+        // Only login after all events are registered otherwise some may be missed.
+        cli.login();
+
+    }
+}
--- src/main/java/com/honeyedoaksoftware/friendlyfeudbot/MessageHandler.java
+++ src/main/java/com/honeyedoaksoftware/friendlyfeudbot/MessageHandler.java
@@ -4,20 +4,45 @@ import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MessageBuilder;
 import sx.blah.discord.util.MissingPermissionsException;
 
 public class MessageHandler {
 
     @EventSubscriber
-    public void OnMesageEvent(MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
+    public void challengeMesageEvent(MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
         IMessage message = event.getMessage();
-        if (message.getContent().startsWith("!challenge")) {
-            sendMessage("Challenge send! Module " + FriendlyFeudBot.moduleName + " is working.", event);
+        if (message.getContent().startsWith(BotUtils.BOT_PREFIX.concat("challenge"))) {
+
+            BotUtils.sendMessage(event.getMessage().getChannel(), "Challenge send! " + BotUtils.BOT_NAME + " is working.");
         }
     }
 
-    public void sendMessage(String message, MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
-        new MessageBuilder(FriendlyFeudBot.client).appendContent(message).withChannel(event.getMessage().getChannel()).build();
+    @EventSubscriber
+    public void ListMesageEvent(MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
+        IMessage message = event.getMessage();
+        if (message.getContent().startsWith(BotUtils.BOT_PREFIX.concat("list"))) {
+
+            BotUtils.sendMessage(event.getMessage().getChannel(), "Challenge List Requested! " + BotUtils.BOT_NAME + " is working.");
+        }
     }
+
+    @EventSubscriber
+    public void winDecideMesageEvent(MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
+        IMessage message = event.getMessage();
+        if (message.getContent().startsWith(BotUtils.BOT_PREFIX.concat("winner"))) {
+
+            BotUtils.sendMessage(event.getMessage().getChannel(), "Challenge winner declared! " + BotUtils.BOT_NAME + " is working.");
+        }
+    }
+
+    @EventSubscriber
+    public void helpMesageEvent(MessageReceivedEvent event) throws DiscordException, MissingPermissionsException {
+        IMessage message = event.getMessage();
+        if (message.getContent().startsWith(BotUtils.BOT_PREFIX.concat("help"))) {
+
+            BotUtils.sendMessage(event.getMessage().getChannel(), BotUtils.BOT_NAME + " help function not implemented yet.");
+        }
+    }
+
+
 }
