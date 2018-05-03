18c89ef1d7785cab5976fd6149555ab6d68c1910
Pieter Van Eeckhout <pieter.vaneeckhout@docbyte.com>
2018-05-03 13:57:23 +0200

    swapping to Command Interface and CommandMap

---
+++ src/main/java/com/honeyedoaksoftware/friendlyfeudbot/command/WinCommand.java
@@ -0,0 +1,15 @@
+package com.honeyedoaksoftware.friendlyfeudbot.command;
+
+import com.honeyedoaksoftware.friendlyfeudbot.util.BotUtils;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+
+import java.util.List;
+
+public class WinCommand implements Command {
+
+    public void runCommand(MessageReceivedEvent event, List<String> args) {
+        IChannel channel = event.getChannel();
+        BotUtils.sendMessage(channel, "Win decided!! " + BotUtils.BOT_NAME + " received the following arguments: " + args);
+    }
+}
